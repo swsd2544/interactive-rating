d799698da4c72152409e6b455eadaf81f51321a5
Siriwudhi Sawaidee <41264368+swsd2544@users.noreply.github.com>
2022-05-26 17:02:33 +0700

    css changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,8 @@ module.exports = {
 			colors: {
 				orange: 'hsl(25, 97%, 53%)',
 				white: 'hsl(0, 0%, 100%)',
-				lightGray: 'hsl(217, 12%, 63%)',
-				mediumGray: 'hsl(216, 12%, 54%)',
-				lightBlue: 'hsl(210, 19%, 19%)',
+				lightGrey: 'hsl(217, 12%, 63%)',
+				mediumGrey: 'hsl(216, 12%, 54%)',
 				darkBlue: 'hsl(213, 19%, 18%)',
 				veryDarkBlue: 'hsl(216, 12%, 8%)',
 			},
